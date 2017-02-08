6ea56c150a64f93de9cb83131e8c927a2e15c728
Lucas Ferreira <lukin_fs@hotmail.com>
2017-02-08 17:16:08 -0200

    Comandos GET, POST, If e Switch

---
+++ php/if_else.php
@@ -0,0 +1,28 @@
+<?php
+
+		// Operadores de Comparação
+
+		// == igual 
+		// === igual do mesmo tipo
+		// != <> diferente
+		// !== diferente, de tipos difentes
+		// <= menor igual
+		// >= maior igual
+
+		// Operadores lógicos
+
+		// && ou AND
+		// || ou OR
+		// XOR -> retorna verdade apenas se umas das expressões for verdadeira
+		// !
+
+	$var1 = 2;
+	$var2 = 2;
+
+	if(!($var1 != $var2)){
+		echo 'Verdade';
+	}else{
+		echo 'Falso';
+	}
+
+?>

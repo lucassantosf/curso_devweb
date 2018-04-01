e747f67b78ea569822692602418d09472a050477
Lucas Ferreira <lukin_fs@hotmail.com>
2018-04-01 20:23:10 -0300

    Configurações Iniciais Projeto Drogaria

---
+++ jsf/Drogaria/src/br/com/drogaria/factory/ConexaoFactory.java
@@ -0,0 +1,28 @@
+package br.com.drogaria.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory {
+	private static final String USUARIO = "root";
+	private static final String SENHA = "";
+	private static final String URL = "jdbc:mysql://localhost:3306/drogaria";
+
+	public static Connection conectar() throws SQLException {
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return conexao;
+	}
+
+	public static void main(String[] args) {
+		try {
+			Connection conexao = ConexaoFactory.conectar();
+			System.out.println("Conexao Realizada com sucesso");
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println("Não foi possivel realizar a conexao");
+		}
+
+	}
+
+}

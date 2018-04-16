2fe983e5bf19e9bf0d06da573ca1367bab41aecd
Lucas Ferreira <lukin_fs@hotmail.com>
2018-04-15 22:08:14 -0300

    Estudo para RestFul

---
+++ jsf/Devmedia/src/br/com/devmedia/dao/DaoUser.java
@@ -0,0 +1,7 @@
+package br.com.devmedia.dao;
+
+import br.com.devmedia.modelo.User;
+
+public class DaoUser extends DaoGenerico<User>{
+
+}
---
+++ jsf/Devmedia/src/br/com/devmedia/modelo/User.java
@@ -0,0 +1,71 @@
+package br.com.devmedia.modelo;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer id;
+
+	private String address;
+
+	private String email;
+
+	private String password;
+
+	@Column(name="user_name")
+	private String userName;
+
+	public User() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}

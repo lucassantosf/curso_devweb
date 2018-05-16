2e455bdbb198cc56b0a3f39a5e75fca33b0ea386
lucassantosf <lucas46em@gmail.com>
2018-05-15 23:33:45 -0300

    Criar nova rota e pagina para cadastros/consultas

--- jsf/AcademiaProject/src/br/com/academiaproject/modelo/Plano.java
+++ jsf/AcademiaProject/src/br/com/academiaproject/modelo/Plano.java
@@ -1,14 +1,10 @@
 package br.com.academiaproject.modelo;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.util.List;
 
+import javax.persistence.*;
 
-/**
- * The persistent class for the plano database table.
- * 
- */
 @Entity
 @NamedQuery(name="Plano.findAll", query="SELECT p FROM Plano p")
 public class Plano implements Serializable {
@@ -20,14 +16,14 @@ public class Plano implements Serializable {
 	private String descplano;
 
 	private double valorplano;
-
-	//bi-directional many-to-one association to Itemvenda
+	
+	/*//bi-directional many-to-one association to Itemvenda
 	@OneToMany(mappedBy="plano")
 	private List<Itemvenda> itemvendas;
-
+	*/
 	public Plano() {
 	}
-
+	
 	public Integer getIdplano() {
 		return this.idplano;
 	}
@@ -50,28 +46,6 @@ public class Plano implements Serializable {
 
 	public void setValorplano(double valorplano) {
 		this.valorplano = valorplano;
-	}
-
-	public List<Itemvenda> getItemvendas() {
-		return this.itemvendas;
-	}
-
-	public void setItemvendas(List<Itemvenda> itemvendas) {
-		this.itemvendas = itemvendas;
-	}
-
-	public Itemvenda addItemvenda(Itemvenda itemvenda) {
-		getItemvendas().add(itemvenda);
-		itemvenda.setPlano(this);
-
-		return itemvenda;
-	}
-
-	public Itemvenda removeItemvenda(Itemvenda itemvenda) {
-		getItemvendas().remove(itemvenda);
-		itemvenda.setPlano(null);
-
-		return itemvenda;
 	}
 
 }
